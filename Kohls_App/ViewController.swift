ebd0d6d097c414f897580242fc65998f2967c369
IMCS2 <IMCS2@imcs1s-macbook-air-2.local>
2019-02-18 01:51:14 -0600

    commit

--- Kohls_App/ViewController.swift
+++ Kohls_App/ViewController.swift
@@ -10,11 +10,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func thirtyTwentyButton(_ sender: Any) {
+        print("30 or 20, How much you want?")
+    }
+    @IBAction func yes2U(_ sender: Any) {
+        print("YES2YOU Rewards Tapped")
+    }
+  
+    @IBAction func camButton(_ sender: Any) {
+      print("Camera Tapped")
+    }
+    
+    
+    @IBAction func barcodeButton(_ sender: Any) {
+        print("Barcode Tapped")
+    }
+    
+    @IBAction func hamburgerOnClick(_ sender: Any) {
+        print("hamburger menu clicked")
+    }
+    
+    
+    @IBAction func shoopingCartOnClick(_ sender: Any) {
+        
+        print("Shopping cart selected")
+    }
+    
+    
+    @IBAction func findAStoreButton(_ sender: Any) {
+        
+        print("FIND A STORE Tapped")
+    }
+    
+    
+    @IBAction func kohlsChargeButton(_ sender: Any) {
+        
+        print("KOHL's CHARGE BUTTON Tapped")
+    }
+    
+    @IBAction func walletButton(_ sender: Any) {
+        
+        print("WALLET Button Tapped")
+    }
+    
+    
+    
+    @IBAction func shopMyStoreButton(_ sender: Any) {
+       print("Shop my Store Tapped")
+    }
+    
+    
+    @IBAction func kohls10button(_ sender: Any) {
+      print("Kohls 10$ cash tapped")
+    
+    }
+    
+    
+    
 }
 
